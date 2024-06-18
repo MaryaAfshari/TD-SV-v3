6ebf97014ebbe0d6609e39876036db68a57da876
afshari-maryam <afshari1431@gmail.com>
2024-06-18 14:59:28 +0300

    Vain train

--- trainECAPAModel2Mine2.py
+++ trainECAPAModel2Mine2.py
@@ -85,7 +85,7 @@ score_file = open(os.path.join(args.save_path, "score.txt"), "a+")
 
 while(1):
     # Training for one epoch
-    if epoch > 35: # I should change it later if I want to train from the base ........5.6.5024
+    if epoch > 0: # I should change it later if I want to train from the base ........5.6.5024
         loss, lr, acc = s.train_network(epoch=epoch, loader=trainLoader)
 
     # Enrollment and Testing every [test_step] epochs
